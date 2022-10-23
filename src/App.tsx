c07fa550204d48b942ceb85493e2ce3ec3429137
swayam-coder <swayam14feb@gmail.com>
2022-10-23 12:46:44 +0530

    added selectedoptions state to context

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar"
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Main from "./Main";
+import { Toaster } from 'react-hot-toast';
+import SelectedOptionsProvider from "./context/SelectedOptions";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#292C33',
+      dark: '#1e2025',
+      light: '#404348',
+      A100: '#282B30',
+      'contrastText': '#878484'
+    },
+    secondary: {
+      main: "#5C61F0",
+      light: "#A5B4FC",
+      dark: "#4338CA",
+    },
+    grey: {
+      '500': "#282B30",
+      '400': "#6D7175"
+    },
+  },
+  breakpoints: {
+    values: {
+        xs: 320,
+        sm: 425,
+        md: 768,
+        lg: 1300,
+        xl: 1410, 
+    }
+  },
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          color: "white"
+        },
+      },
+    },
+    MuiSelect: {
+      styleOverrides: {
+        icon: {
+          color: 'white'
+        }
+      }
+    }
+  },
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SelectedOptionsProvider>
+      <ThemeProvider theme={theme}>
+        <Navbar />
+        <Main />
+        <Toaster
+          containerStyle={{
+            top: 30,
+            left: 30,
+            bottom: 30,
+            right: 30,
+            fontFamily: "Roboto, Helvetica, Arial ,sans-serif"
+          }}
+          position='top-right'
+        />
+      </ThemeProvider>
+    </SelectedOptionsProvider>
   );
 }
 
