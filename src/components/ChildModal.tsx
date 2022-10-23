d57ecaed8fe46cf6e6dd92b9e9e79333bc20e1ab
swayam-coder <swayam14feb@gmail.com>
2022-10-23 17:11:26 +0530

    inlined state updating callbacks

--- src/components/ChildModal.tsx
+++ src/components/ChildModal.tsx
@@ -6,27 +6,19 @@ import { ChildQueryBuilderModal, CloseModal } from '../styled-components/QueryBu
 export default function ChildModal({ buttonText, btncolor, children }: { buttonText: string, btncolor: string, children: React.ReactNode }) {
     const [open, setOpen] = React.useState(false);
 
-    const handleOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-  
     return (
       <React.Fragment>
-        <Button sx={{ backgroundColor: btncolor, color: 'white' }} onClick={handleOpen}>{buttonText}</Button>
+        <Button sx={{ backgroundColor: btncolor, color: 'white' }} onClick={() => setOpen(true)}>{buttonText}</Button>
         <ChildQueryBuilderModal
             hideBackdrop
             open={open}
-            onClose={handleClose}
+            onClose={() => setOpen(false)}
         >
             <React.Fragment>
                 <Grid container>
                     {children}
                 </Grid>
-                <CloseModal sx={{ cursor: 'pointer' }} titleAccess='Close Query Builder' onClick={handleClose} />
+                <CloseModal sx={{ cursor: 'pointer' }} titleAccess='Close Query Builder' onClick={() => setOpen(false)} />
             </React.Fragment>
         </ChildQueryBuilderModal>
       </React.Fragment>
