bb7624c5d7952e87f8870184255e136e489580ca
swayam-coder <swayam14feb@gmail.com>
2022-10-23 13:01:46 +0530

    blank placeholder paper for main page

--- src/styled-components/PaperItem.ts
+++ src/styled-components/PaperItem.ts
@@ -1,13 +1,9 @@
 import { PaletteColor, styled as MuiStyled, Theme } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 
-const Item = MuiStyled(Paper)(({ theme }) => ({
+export const PlaceHolderPaper = MuiStyled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     margin: "30px 0",
     position: 'relative',
     backgroundColor: theme.palette.primary.main
-}));
-
-export const PlaceHolderPaper = MuiStyled(Item)(({ theme }) => ({
-    
-}));
+}))
