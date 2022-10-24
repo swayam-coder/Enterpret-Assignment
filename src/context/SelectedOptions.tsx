73637d5afeb44e48985c4186da3fdf931ac9238a
swayam-coder <swayam14feb@gmail.com>
2022-10-24 09:18:35 +0530

    selectedoptions reducers

--- src/context/SelectedOptions.tsx
+++ src/context/SelectedOptions.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { selectedOptionsReducer } from '../reducers/selectedoptions.reducer'
+import { selectedOptionsReducer } from '../reducers/selectedoptions'
 import { defaultOptions } from '../utils/data'
 import { createContext, useContext } from 'react';
 
